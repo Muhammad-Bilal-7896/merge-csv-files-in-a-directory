dec03a4121c43d3d3ae9ee636d08867d984846f7
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-01-13 23:02:53 +0500

    feat

--- index.py
+++ index.py
@@ -1,53 +1,54 @@
 # All files and directories ending with .txt and that don't begin with a dot:
+import numpy as np
 import os
 import pandas as pd
-#Get the list of all files and directories
-
-path = "H://sir zeeshan work//test//0"
+# Get the list of all files and directories
+# H://sir zeeshan work//Coding files//Test
+path = "H://sir zeeshan work//Coding files//Test"
 dir_list = os.listdir(path)
- 
+
 #print("Files and directories in '", path, "' :")
 
 # Length of directory
 print(len(dir_list))
 length = len(dir_list)
 
-import numpy as np
 
-#Greater loop to go through each file name
-count=0
-Greater_Longitude=[]
+# Greater loop to go through each file name
+count = 0
+Greater_Longitude = []
 Greater_Latitude = []
 Greater_Date = []
 Greater_NDVI = []
 Greater_Product = []
 
-############################################We are inside the loop.Get ready the loop is going to start
+# We are inside the loop.Get ready the loop is going to start
 for files in dir_list:
-  #print(files)
-  data = pd.read_csv('H:/sir zeeshan work/test/0/'+str(files), encoding="ISO-8859-1")
-  #print(files)
-  
-  longitude= data['longitude'].tolist()
-  latitude = data['latitude'].tolist()
-  date = data['date'].tolist()
-  NDVI = data['NDVI'].tolist()
-  product = data['product'].tolist()
-  #########################APPENDING THE DATA#######################################
-  Greater_Longitude.append(longitude)
-  Greater_Latitude.append(latitude)
-  Greater_Date.append(date)
-  Greater_NDVI.append(NDVI)
-  Greater_Product.append(product)
-  
-  #Printing the count to know at which length the memory leak is occuring
-  #print("Count =====================> ",count)
+    # print(files)
+    data = pd.read_csv('H:/sir zeeshan work/Coding files/Test/' +
+                       str(files))
+    # print(files)
 
-  #Breaking the loop to avoid memory leak when the files length available in the folder or directory are reached.Discovered to avoid memory leak
-  count = count + 1
-  if(count==length-1):
-      break
-############################################The loop Ended Successfully.Done
+    longitude = data['longitude'].tolist()
+    latitude = data['latitude'].tolist()
+    date = data['date'].tolist()
+    NDVI = data['NDVI'].tolist()
+    product = data['product'].tolist()
+    #########################APPENDING THE DATA#######################################
+    Greater_Longitude.append(longitude)
+    Greater_Latitude.append(latitude)
+    Greater_Date.append(date)
+    Greater_NDVI.append(NDVI)
+    Greater_Product.append(product)
+
+    # Printing the count to know at which length the memory leak is occuring
+    #print("Count =====================> ",count)
+
+    # Breaking the loop to avoid memory leak when the files length available in the folder or directory are reached.Discovered to avoid memory leak
+    count = count + 1
+    if(count == length-1):
+        break
+# The loop Ended Successfully.Done
 
 # 2) Reading the excel file
 
@@ -64,12 +65,36 @@ for files in dir_list:
 # NDVI = data['NDVI'].tolist()
 # product = data['product'].tolist()
 
-#Merging all arrays
-Greater_Longitude_Combine = np.concatenate((Greater_Longitude))
- 
-# #printing list data
-print('longitude : ',len(Greater_Longitude_Combine))
-# print('latitude: ', latitude)
-# print('date :', date)
-# print('NDVI :', NDVI)
-# print('product :', product)
+# Merging all arrays
+Greater_Longitude_Combined = np.concatenate((Greater_Longitude))
+Greater_Latitude_Combined = np.concatenate((Greater_Latitude))
+Greater_Date_Combined = np.concatenate((Greater_Date))
+Greater_NDVI_Combined = np.concatenate((Greater_NDVI))
+Greater_Product_Combined = np.concatenate((Greater_Product))
+
+# printing list data
+print('Length of longitude Array is : ', len(Greater_Longitude_Combined))
+print('Length of latitude Array is : ', len(Greater_Latitude_Combined))
+print('Length of date Array is :', len(Greater_Date_Combined))
+print('Length of NDVI Array is :', len(Greater_NDVI_Combined))
+print('Length of product Array is :', len(Greater_Product_Combined))
+
+
+# Printing finally as arrays
+print('Length of longitude Array is : ', type(Greater_Longitude_Combined))
+print('Length of latitude Array is : ', len(Greater_Latitude_Combined))
+print('Length of date Array is :', len(Greater_Date_Combined))
+print('Length of NDVI Array is :', len(Greater_NDVI_Combined))
+print('Length of product Array is :', Greater_Product_Combined)
+
+####################Writing data finally to csv file
+df = pd.DataFrame(
+    {
+        "longitude": Greater_Longitude_Combined,
+        "latitude": Greater_Latitude_Combined,
+        "date": Greater_Date_Combined,
+        "NDVI": Greater_NDVI_Combined,
+        "product": Greater_Product_Combined
+    }
+)
+df.to_csv("Test All 6 Combined.csv", index=False)
